c3b4dba10efc988c5b14cca8758745217d8d1570
Arabella Moss <ellamoss@protonmail.com>
2023-11-18 17:19:14 +0000

    save changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,32 +4,27 @@ import Navbar from "./components/Navbar";
 import Mission from "./components/Mission";
 import Challenge from "./components/Challenge";
 import Share from "./components/Share";
-import video from "./video/background-video.mp4";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <video autoPlay loop muted>
-          <source src={video} type="video/mp4" />
-        </video>
-        <div className="title">
+      <div className="title">
+        <div style={{ width: '100%', height: '100%', position: 'relative', background: 'white' }}>
           <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
-            <h1 style={{ fontSize: '6rem' }}>Ecolab</h1> 
-            <div className="paragraph">
-              <p><b>ECOLAB is a platform that was established to empower the public to create innovative design solutions by, for, and with nature while advancing humanity.</b> </p>
-            </div>
           </Link>
-          <Navbar />
-          <Switch>
-            <Route path="/mission" component={Mission} />
-            <Route path="/challenge" component={Challenge} />
-            <Route path="/share" component={Share} />
-            <Route path="/" exact>
-            </Route>
-          </Switch>
         </div>
+        
+          
+        
+        <Navbar />
+        <Switch>
+          <Route path="/mission" component={Mission} />
+          <Route path="/challenge" component={Challenge} />
+          <Route path="/share" component={Share} />
+          <Route path="/" exact>
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,23 @@
-import { Link } from "react-router-dom"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
-
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav>
-      <ul>
-      <li><Link to="/share">SHARE</Link></li>
-        <li><Link to="/Challenge">CHALLENGE</Link></li>
-        <li><Link to="/mission">MISSION</Link></li>
-      </ul>
+    <nav className="navbar">
+      <h1>Ecolab</h1>
+      <div className="links">
+        <ul>
+          <li><Link to="/mission">Mission</Link></li>
+          <li><Link to="/challenge">Challenge</Link></li>
+          <li><Link to="/share">Share</Link></li>
+        </ul>
+      </div>
+      <div className="bio">
+        <p><b>ECOLAB is a platform that was established to empower the public to create innovative design solutions by, for, and with nature while advancing humanity.</b></p>
+      </div>
     </nav>
   );
 }
 
 export default Navbar;
-
