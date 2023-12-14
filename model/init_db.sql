a210ce138e36f32832f7e3623feab4685b796eea
Zoe Laventhol <zoe.laventhol@gmail.com>
2023-12-14 21:15:50 +0100

    init db debugging

--- model/init_db.sql
+++ model/init_db.sql
@@ -12,17 +12,17 @@ SET foreign_key_checks = 1;
 --
 
 CREATE TABLE users(
-    id INT NOT NULL AUTO_INCREMENT, 
-    email VARCHAR(200) NOT NULL, 
-    password VARCHAR(200) NOT NULL, 
-    organization VARCHAR(200),
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(40) NOT NULL,
+    password VARCHAR(40) NOT NULL, 
+    organization VARCHAR(60) NOT NULL,
     PRIMARY KEY (id)
     );
 
 CREATE TABLE designs(
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT,
-    file_name VARCHAR(100) NOT NULL,
+    file_name VARCHAR(60) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
     );
@@ -31,4 +31,5 @@ CREATE TABLE designs(
 -- Insert some data
 --
 INSERT INTO users (email, password, organization)
+VALUES ('cminnich93', 'cammie1031', 'CodeOp')
 VALUES ('cminnich93', 'cammie1031', 'CodeOp')
